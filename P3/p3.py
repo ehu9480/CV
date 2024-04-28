127753f3a667d81e48018006dca30bc49f6283d1
ehu9480 <86241687+ehu9480@users.noreply.github.com>
2024-04-27 21:59:43 -0400

    optimized

--- P3/p3.py
+++ P3/p3.py
@@ -7,19 +7,16 @@ from scipy import ndimage, signal
 
 ### TODO 1: Read an Image and convert it into a floating point array with values between 0 and 1. You can assume a color image
 def imread(filename):
-    img = Image.open(filename).convert('RGB')
-    img = np.array(img).astype(float)
-    img = img / 255.
+    img = np.array(Image.open(filename).convert('RGB'), dtype=float) / 255.0
     return img
 
 ### TODO 2: Create a gaussian filter of size k x k and with standard deviation sigma
 def gaussian_filter(k, sigma):
-    f = np.zeros((k,k))
-    center = k//2
-    for i in range(k):
-        for j in range(k):
-            f[i,j] = np.exp(-(float(i-center)**2 + float(j-center)**2)/(2*sigma*sigma))
-    f = f/np.sum(f)
+    x = np.arange(-k//2, k//2 + 1)
+    y = np.arange(-k//2, k//2 + 1)
+    xx, yy = np.meshgrid(x, y)
+    f = np.exp(-(xx**2 + yy**2) / (2 * sigma**2))
+    f = f / np.sum(f)
     return f
 
 ### TODO 3: Compute the image gradient. 
@@ -29,10 +26,10 @@ def gaussian_filter(k, sigma):
 ### Convolve with [0.5, 0, -0.5] to get the X derivative on each channel and convolve with [[0.5],[0],[-0.5]] to get the Y derivative on each channel. (use scipy.signal.convolve) 
 ### Return the gradient magnitude and the gradient orientation (use arctan2)
 def gradient(img):
-    img = 0.2125*img[:,:,0] + 0.7154*img[:,:,1] + 0.0721*img[:,:,2]
-    img = signal.convolve(img, gaussian_filter(5,1), mode='same')
-    gx = signal.convolve(img, np.array([[0.5, 0, -0.5]]), mode='same')
-    gy = signal.convolve(img, np.array([[0.5],[0],[-0.5]]), mode='same')
+    gray_img = 0.2125*img[:,:,0] + 0.7154*img[:,:,1] + 0.0721*img[:,:,2]
+    smoothed_img = signal.convolve(gray_img, gaussian_filter(5,1), mode='same')
+    gx = signal.convolve(smoothed_img, np.array([[0.5, 0, -0.5]]), mode='same')
+    gy = signal.convolve(smoothed_img, np.array([[0.5],[0],[-0.5]]), mode='same')
     mag = np.sqrt(gx*gx + gy*gy)
     ori = np.arctan2(gy, gx)
     return mag, ori
@@ -50,12 +47,10 @@ def check_distance_from_line(x, y, theta, c, thresh):
 ### where every pixel which is less than thresh units away from the line should be colored red
 def draw_lines(img, lines, thresh):
     img = img.copy()
-    I, J = np.unravel_index(np.arange(img.shape[0]*img.shape[1]), (img.shape[0], img.shape[1]))
+    I, J = np.indices(img.shape[:2])
     for (theta, c) in lines:
-        idx = np.where(check_distance_from_line(J, I, theta, c, thresh))[0]
-        img[I[idx], J[idx],0] = 1
-        img[I[idx], J[idx],1] = 0
-        img[I[idx], J[idx],2] = 0
+        dist = np.abs(J * np.cos(theta) + I * np.sin(theta) + c) / np.sqrt(1 + np.cos(theta)**2 + np.sin(theta)**2)
+        img[dist < thresh] = [1, 0, 0]
     return img
  
 ### TODO 6: Do Hough voting. You get as input the gradient magnitude and the gradient orientation, as well as a set of possible theta values and a set of possible c
@@ -64,36 +59,21 @@ def draw_lines(img, lines, thresh):
 ### (b) Its distance from the (theta, c) line is less than thresh2, and
 ### (c) The difference between theta and the pixel's gradient orientation is less than thresh3
 def hough_voting(gradmag, gradori, thetas, cs, thresh1, thresh2, thresh3):
-    bmap = gradmag>thresh1
+    bmap = gradmag > thresh1
     I, J = np.where(bmap)
     votes = np.zeros((len(thetas), len(cs)))
-    for i in range(len(thetas)):
-        for j in range(len(cs)):
-            theta = thetas[i]
-            c = cs[j]
+    for i, theta in enumerate(thetas):
+        for j, c in enumerate(cs):
             val = check_distance_from_line(J, I, theta, c, thresh2)
-            ori = np.abs(gradori[I,J] - theta)
-            votes[i,j] = votes[i,j] + np.sum(val & (ori<thresh3))
+            ori = np.abs(gradori[I, J] - theta)
+            votes[i, j] = np.sum(val & (ori < thresh3))
     return votes
     
 ### TODO 7: Find local maxima in the array of votes. A (theta, c) pair counts as a local maxima if (a) its votes are greater than thresh, and 
 ### (b) its value is the maximum in a (nbhd x nbhd) neighborhood in the votes array.
 ### Return a list of (theta, c) pairs
-def localmax(votes, thetas, cs, thresh,nbhd):
-    chosenI = []
-    chosenJ = []
-    k=nbhd//2
-    for i in range(0,votes.shape[0]):
-        for j in range(0, votes.shape[1]):
-            lowi = max(0, i-k)
-            lowj = max(0,j-k)
-            highi = min(votes.shape[0], i+k+1)
-            highj = min(votes.shape[1], j+k+1)
-            if (votes[i,j]>thresh) and (votes[i,j] >= np.max(votes[lowi:highi,lowj:highj])):
-                chosenI.append(i)
-                chosenJ.append(j)
-    chosenI = np.array(chosenI)
-    chosenJ = np.array(chosenJ)
+def localmax(votes, thetas, cs, thresh, nbhd):
+    chosenI, chosenJ = np.where((votes > thresh) & (votes == ndimage.maximum_filter(votes, size=nbhd)))
     return list(zip(thetas[chosenI], cs[chosenJ]))
 
 # Final product: Identify lines using the Hough transform    
